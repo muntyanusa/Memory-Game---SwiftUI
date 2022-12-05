c92b8a848c6f7e78ec509ec08acce7b4f1d1b123
Stanislav Muntyanu <stasean21@yandex.com>
2022-12-05 14:33:51 +0300

    Initial Commit

---
+++ MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by Станислав Мунтяну on 05.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
